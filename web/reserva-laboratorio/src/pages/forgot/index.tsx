60850c9495019d7110e91a087509302ca7018987
Lucas Furtado <lucas_shiguioka@hotmail.com>
2020-08-31 13:18:14 -0300

    finished styles for login e forgot forms

--- web/reserva-laboratorio/src/pages/forgot/index.tsx
+++ web/reserva-laboratorio/src/pages/forgot/index.tsx
@@ -1,30 +1,41 @@
-import React from  'react'
+import React, { useState } from  'react'
 import { Link } from "react-router-dom";
 import './styles.css'
-function forgotPassword(){
+function ForgotPassword(){
+
+    const [email,emailState] = useState('')
+
+    function handleSendMail(event:any){
+        const element:any = document.querySelector("#critica")
+        const eventElement:any = event.target
+        element.classList.toggle('hide')
+        eventElement.classList.toggle('hide')
+    }
+
     return (
         <div id="forgot-box" className="container">
             <header>
                 <Link to="/">
                     Voltar
                 </Link>
-                <h3>Esqueci minha senha</h3>
             </header>
             <main>
                 <label htmlFor="mail">Informe Seu Email</label>
                 <input 
                     type="text"
                     id="mail"
-                    onChange={e=>console.log('checked')}
+                    onChange={e=>emailState(e.target.value)}
                 />
             </main>
             <footer>
-                <div id="critica">
-                    Enviamos um email contendo uma senha provisoriabr
-                    Em seu porximo login efetue a troca do mesmo
+                <div id="critica" className="hide">
+                    Enviamos um email contendo uma senha provisoria,
+                    em seu proximo login efetue a troca de sua senha, 
+                    a validade de suasenha provisória é de 1 login
                 </div>
                 <button
                     type="button"
+                    onClick={e => handleSendMail(e)}
                 >
                     Recuperar Senha
                 </button>
@@ -33,4 +44,4 @@ function forgotPassword(){
     )
 }
 
-export default forgotPassword
+export default ForgotPassword
--- web/reserva-laboratorio/src/routes.tsx
+++ web/reserva-laboratorio/src/routes.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter,Route,Switch } from "react-router-dom";
 import Login from "./pages/login";
-import forgotPassword from "./pages/forgot";
+import ForgotPassword from "./pages/forgot";
 
 function Routes (){
     return(
         <BrowserRouter>
             <Switch>
                 <Route exact={true} path="/" component={Login} />
-                <Route exact={true} path="/forgot" component={forgotPassword} />
+                <Route exact={true} path="/forgot" component={ForgotPassword} />
             </Switch>
         </BrowserRouter>
     )
