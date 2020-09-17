a1e29bf2a5bfe71ec4d6b97cd7da4b64b939dedd
Lucas Furtado <lucas_shiguioka@hotmail.com>
2020-09-17 12:40:11 -0300

    local alteratiosn

--- backend/src/controllers/locationUserController.ts
+++ backend/src/controllers/locationUserController.ts
@@ -4,24 +4,25 @@ import LocationUserModel from '../models/locationUserModel';
 
 class LocationUserController{
 
-    private model = new LocationUserModel()
-
     async assign(req:Request,res:Response){
         const {locationId,userId} = req.body
-        const {message,error} = await this.model.new({locationId,userId})
+        const model = new LocationUserModel()
+        const {message,error} = await model.new({locationId,userId})
         return error ? res.json(message) : res.status(404).json(error)
     }
 
     async unassign(req:Request,res:Response){
         const {id} = req.body
-        const {message,error} = await this.model.delete(id)
+        const model = new LocationUserModel()
+        const {message,error} = await model.delete(id)
         return error ? res.json(message) : res.status(404).json(error)
     }
 
     async listAssigns(req:Request,res:Response){
         const {userId} = req.params
         const id = Number(userId )
-        const {listOfUserLocatios,error} = await this.model.list(id)
+        const model = new LocationUserModel()
+        const {listOfUserLocatios,error} = await model.list(id)
         return error ? res.status(404).json(error) : res.json(listOfUserLocatios)
     }
 }
