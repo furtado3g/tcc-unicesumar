e0452c491efe2b13846760c5dcc02f136fcb5b83
Lucas Furtado <lucas_shiguioka@hotmail.com>
2020-10-01 23:24:27 -0300

    admin form to new User

--- web/reserva-laboratorio/src/pages/admin/index.tsx
+++ web/reserva-laboratorio/src/pages/admin/index.tsx
@@ -1,9 +1,14 @@
 import React, { useState } from 'react'
+import { Link } from "react-router-dom";
 import Sidebar from '../../components/sidebar'
 import './styles.css'
 function Admin(){
 
+    const [loggedUsername, loggedUsernameState] = useState('')
+    const [name, nameState] = useState('')
     const [username, usernameState] = useState('')
+    const [password, passwordState] = useState('')
+    const [redundacy, redundacyState] = useState('')
 
     async function handleWithPageLoad(){
         const requestData = {
@@ -16,14 +21,17 @@ function Admin(){
                 },
             }
         }
-        const request = await fetch(requestData.url,requestData.options)
-        if(request.status === 200) {
-            const response = await request.json()
-            const { name } = response 
-            usernameState(name)
-        }else{
-            usernameState('Administrador')
-        }
+        await fetch(requestData.url,requestData.options)
+        .then(data =>{
+          return data.json()  
+        })
+        .then((data:any) =>{
+            const { name } = data 
+            loggedUsernameState(name)
+        })
+        .catch(e=>{
+            loggedUsernameState("Admin")
+        })
     }
 
     useState(handleWithPageLoad())
@@ -35,13 +43,92 @@ function Admin(){
                 <div className="panel">
                     <div className="panel-header">
                         <div className="panel-title">
-                            {username}
+                            {loggedUsername}
                         </div>
                     </div>
                     <div className="panel-body">
                         <div className="panel-sidebar">
                             <div className="panel-sidebar-item">
                                 Novo Usuario
+                            </div>
+                        </div>
+                        <div className="panel-content">
+                            <div className="row">
+                                <h2 className="page-name">
+                                    Novo Usuario
+                                </h2>
+                            </div>
+                            <div className="row">
+                                <div className="col-6">
+                                    <label htmlFor="name">Nome</label>
+                                    <input 
+                                        type="text"
+                                        className="form-control"
+                                        id="name"
+                                        onChange={e=>nameState(e.target.value)}
+                                    />
+                                </div>
+                                <div className="col-6">
+                                    <label htmlFor="userName">Login</label>
+                                    <input 
+                                        type="text"
+                                        className="form-control"
+                                        id="userName"
+                                        onChange={e=>usernameState(e.target.value)}
+                                    />
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-6">
+                                    <label htmlFor="password">Senha</label>
+                                    <input 
+                                        type="password"
+                                        className="form-control"
+                                        id="password"
+                                        onChange={e=>passwordState(e.target.value)}
+                                    />
+                                </div>
+                                <div className="col-6">
+                                    <label htmlFor="c_password">Confirmar Senha</label>
+                                    <input 
+                                        type="password"
+                                        className="form-control"
+                                        id="c_password"
+                                        onChange={e=>redundacyState(e.target.value)}
+                                    />
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-12">
+                                    <select 
+                                        className="form-control" 
+                                        id="userType"
+                                    >
+                                        <option value="">Selecione</option>
+                                        <option value="1">Coordenador</option>
+                                        <option value="2">Professor</option>
+                                        <option value="3">Laboratorista</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-12 text-center" >
+                                    <button
+                                        className="btn btn-success"
+                                    >
+                                        Salvar Usuario
+                                    </button>
+                                    <Link 
+                                        to="/admin"
+                                    >
+                                        
+                                        <button
+                                            className="btn btn-danger"
+                                        >
+                                        Voltar
+                                        </button>
+                                    </Link>
+                                </div>
                             </div>
                         </div>
                     </div>
