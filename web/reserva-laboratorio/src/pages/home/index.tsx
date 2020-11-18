951ded087ee7b9bed6dc474191038b0e15480e3d
Lucas Furtado <lucas_shiguioka@hotmail.com>
2020-11-17 21:42:31 -0300

    initialize agenda

--- web/reserva-laboratorio/src/pages/home/index.tsx
+++ web/reserva-laboratorio/src/pages/home/index.tsx
@@ -1,21 +1,24 @@
 import React, { useEffect } from 'react'
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
 import Sidebar from '../../components/sidebar/index'
 import moment from 'moment'
-import {useHistory} from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 import { useToasts } from 'react-toast-notifications'
+import Panel from '../../components/panel'
 
 function Home() {
   const sessionToken = localStorage.getItem("sessionToken")
   const expires_at = localStorage.getItem("expires_at")
-  const{addToast} = useToasts();
+  const { addToast } = useToasts();
   const History = useHistory();
   useEffect(() => {
     if (sessionToken == null) {
-      addToast("É necessário estar logado para obter acesso ao sistema", {appearance: 'warning', autoDismiss: true})
+      addToast("É necessário estar logado para obter acesso ao sistema", { appearance: 'warning', autoDismiss: true })
       History.push('/')
     }
     if (moment(expires_at) < moment()) {
-      addToast("Sessão expirada", {appearance: 'warning', autoDismiss: true})
+      addToast("Sessão expirada", { appearance: 'warning', autoDismiss: true })
       History.push('/')
     }
   }, [])
@@ -23,6 +26,13 @@ function Home() {
 
     <div className="container-admin">
       <Sidebar />
+      <div className="panel">
+        <FullCalendar
+          plugins={[dayGridPlugin]}
+          initialView="dayGridMonth"
+        />
+      </div>
+
     </div>
   )
 }
